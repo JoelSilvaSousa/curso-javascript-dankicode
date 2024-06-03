37c49113502e8f69b201d51960568e88b9eb3ca1
Joel Silva Sousa <sousajoelsilva@gmail.com>
2024-06-03 08:44:05 -0300

    Atualização

--- supermercado/supermercado.js
+++ supermercado/supermercado.js
@@ -2,13 +2,38 @@ var items = [];
 
 document.querySelector('input[type=submit]').
 addEventListener('click',()=>{
-    var nomeProduto = document.querySelector('input[name=nome_produto]').value;
-    var precoProduto = document.querySelector('input[name=valor_produto]').value;
+    var nomeProduto = document.querySelector('input[name=nome_produto]');
+    var precoProduto = document.querySelector('input[name=price]');
 
     items.push({
         nome: nomeProduto,
         valor: precoProduto
     })
 
-    alert(items[0].nome);
-})
+    /*
+    
+
+    */
+        let listaProduto = document.querySelector('.lista-produto');
+        let soma = 0;
+        listaProduto.innerHTML="";
+        items.map(function(val){
+            soma+=parseFloat(val.valor);
+            listaProduto.innerHTML +=`
+                <div class="lista-produto-single">
+                    <h3>`+val.nome+`</h3>
+                    <h3 class="price-produto"><span>R$`+val.valor+`</span></h3>
+                 </div>
+            `;
+        })
+
+        soma = soma.toFixed(2);
+    nomeProduto.value = "";
+    precoProduto.value = "";
+
+    let elementoSoma = document.querySelector('.soma-produto h1');
+    elementoSoma.innerHTML = 'R$'+soma;
+
+        
+    
+});
